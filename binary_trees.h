b23edec19379c5fb56d576c3bcb61b5b615b338e
Ninikay <ms.nikeoni@gmail.com>
2022-08-23 13:33:59 +0000

    updating bt.h3

--- binary_trees.h
+++ binary_trees.h
@@ -43,5 +43,8 @@ typedef struct link_s
 /*  ----------------- mandatory task ---------------------------------*/
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
 
 #endif /* _BINARY_TREES_H_ */
